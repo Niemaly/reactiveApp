a630984a5b17c6a2cb1cfa39f725a8ebf94fdc82
Niemaly <jac3k2@gmail.com>
2020-05-06 21:51:40 +0200

    'testing'

--- src/test/java/com/example/reactiveApi/ReactiveApiApplicationTests.java
+++ src/test/java/com/example/reactiveApi/ReactiveApiApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.reactiveApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactiveApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Flux.just("Jacek", "Fiora", "Krzysztof", "Franciszek", "Paweł", "Justyna", "Ewelina", "Nadia", "Grzegorz", "Wacław", "Iwona", "Dominika").
+				filter(element-> element.startsWith("J")).
+		subscribe(System.out::println);                                     // it takes many elements
+		//   Mono.just(""); // it takes only one element
+
 	}
 
 }
